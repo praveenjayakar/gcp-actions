ac12056d8be036f32ec34ec2a960305183f208cf
Praveen jayakar <49543930+praveenjayakar@users.noreply.github.com>
2026-02-15 14:45:01 +0530

    Add Terraform configuration for Google Compute Instance

---
+++ main.tf
@@ -0,0 +1,33 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+
+
+resource "google_compute_instance" "linux_vm" {
+  name         = "ai-agent-linux-vm"
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "projects/debian-cloud/global/images/family/debian-12"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  tags = ["ai-agent", "linux"]
+}
+
+
+output "vm_public_ip" {
+  value = google_compute_instance.linux_vm.network_interface[0].access_config[0].nat_ip
+}
+
+
